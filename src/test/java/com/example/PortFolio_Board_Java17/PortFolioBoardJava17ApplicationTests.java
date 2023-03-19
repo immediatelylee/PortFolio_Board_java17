747a2f2bfdc230c69eb72a1452a9f38bf90815aa
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2023-03-20 02:28:05 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은:
    
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복
    * configuration-processor

---
+++ src/test/java/com/example/PortFolio_Board_Java17/PortFolioBoardJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PortFolio_Board_Java17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortFolioBoardJava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
