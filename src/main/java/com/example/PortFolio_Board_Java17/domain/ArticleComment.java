e078e2bf399ed52624c12d12d3783908a523f73f
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2023-03-25 01:27:39 +0900

    #1 - 각 파일의 EOF 제거 및 Optimize import

--- src/main/java/com/example/PortFolio_Board_Java17/domain/ArticleComment.java
+++ src/main/java/com/example/PortFolio_Board_Java17/domain/ArticleComment.java
@@ -1,17 +1,11 @@
 package com.example.PortFolio_Board_Java17.domain;
 
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
@@ -56,6 +50,7 @@ public class ArticleComment extends AuditingFields {
     }
 
 }
+
 
 
 
