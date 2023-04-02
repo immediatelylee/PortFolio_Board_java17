8f7f9e679a8414ba114b7308160f22bcbc1ae8de
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2023-04-03 03:08:02 +0900

    #24 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    나머지 로직의 테스트 코드를 작성하고
    연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- src/main/java/com/example/PortFolio_Board_Java17/service/ArticleService.java
+++ src/main/java/com/example/PortFolio_Board_Java17/service/ArticleService.java
@@ -2,10 +2,11 @@ package com.example.PortFolio_Board_Java17.service;
 
 import com.example.PortFolio_Board_Java17.domain.type.SearchType;
 import com.example.PortFolio_Board_Java17.dto.ArticleDto;
-import com.example.PortFolio_Board_Java17.dto.ArticleUpdateDto;
+import com.example.PortFolio_Board_Java17.dto.ArticleWithCommentsDto;
 import com.example.PortFolio_Board_Java17.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,19 +18,19 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String search_keyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long l) {
+    public ArticleWithCommentsDto getArticle(Long articleId) {
         return null;
     }
 
     public void saveArticle(ArticleDto dto) {
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
     }
 
     public void deleteArticle(long articleId) {
