07c72cf59888099a8dbd8b9ee2386b2e1adf2fa1
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2023-04-23 15:27:44 +0900

    #41 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId`로 id를 변경하는 내용을
    모든 자바코드에 반영

--- src/main/java/com/example/PortFolio_Board_Java17/repository/UserAccountRepository.java
+++ src/main/java/com/example/PortFolio_Board_Java17/repository/UserAccountRepository.java
@@ -4,6 +4,6 @@ package com.example.PortFolio_Board_Java17.repository;
 import com.example.PortFolio_Board_Java17.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
 
