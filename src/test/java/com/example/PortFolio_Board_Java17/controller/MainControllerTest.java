74bacb3f1293cbd80756429a73ed2471566b0589
immediatelylee <50638197+immediatelylee@users.noreply.github.com>
2023-05-07 20:19:49 +0900

    수정되어야 할 코드들 변경
    Securityconfig TestSecurityConfig 으로 변경
    누락되어있던 service들 @MockBean으로 등록
    
    * then import할때 BDDAssertion으로 등록하지 않도록 주의

--- src/test/java/com/example/PortFolio_Board_Java17/controller/MainControllerTest.java
+++ src/test/java/com/example/PortFolio_Board_Java17/controller/MainControllerTest.java
@@ -1,23 +1,23 @@
 package com.example.PortFolio_Board_Java17.controller;
 
-import com.example.PortFolio_Board_Java17.config.SecurityConfig;
+import com.example.PortFolio_Board_Java17.config.TestSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(MainController.class)
 class MainControllerTest {
     private final MockMvc mvc;
     public MainControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
-
+    @DisplayName("[view][GET] 루트 페이지 -> 게시글 리스트 (게시판) 페이지 Redirection")
     @Test
     void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
         //Given
@@ -26,7 +26,7 @@ class MainControllerTest {
         mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("forward:/articles"))
-                .andExpect(forwardedUrl("/articles"))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(forwardedUrl("/articles"));
+
     }
 }
